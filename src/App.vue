e1e330be8fcfad5a9acc79c9743d1e7165875da9
changsoonyun <changsoonyun@naver.com>
2022-11-04 21:22:59 +0900

    .

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
-  <div>
-    <h4 :style="스타일">XX 원룸</h4>
-    <p>XX 만원</p>
-  </div>
-  <div>
-    <h4>XX 원룸</h4>
-    <p>XX 만원</p>
+  <div class="menu">
+    <a v-for="(작명, i) in 메뉴들" :key="i">{{ 작명 }}</a>
   </div>
 </template>
 
@@ -14,20 +9,20 @@ export default {
   name: "App",
   data() {
     return {
-      price1: 60,
-      스타일: "color:red",
+      메뉴들: ["Home", "Shop", "About"],
     };
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
