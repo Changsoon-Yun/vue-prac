e80fe56e8556ce3d159877115092d3c6a116db77
changsoonyun <changsoonyun@naver.com>
2022-11-10 21:07:11 +0900

    create card component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,18 @@
 <template>
-<div>
-  asda
-</div>
+  <div
+      class="content"
+      @click="
+      setModal();
+      index = i;
+    "
+      v-for="(a, i) in products"
+      :key="i"
+  >
+    <img :src="a.image" alt="" />
+    <h4>{{ a.title }}</h4>
+    <p>{{ a.content }}</p>
+    <p>{{ a.price }}원</p>
+  </div>
 </template>
 
 <script>
